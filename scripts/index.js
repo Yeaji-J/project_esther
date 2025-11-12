95262c173dfa0fcb162d91bc1b7271cacad35c63
주예지 <juyeaji@juyejiui-iMac.local>
2025-11-13 01:17:22 +0900

    에스더몰 js -  swiper pagination, btn 작업중

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
-//swiper pagination, btn
-//서브메뉴 클릭 시 swiper 자체 변화,,, 추가 swiper 제작,, 애니메이션
+//서브메뉴 클릭 시 swiper 자체 변화,,, 추가 swiper 제작,, 애니메이션 e.preventDefault()
 //검색 아이콘 클릭 시 팝업?!
-//scrollTrigger -> 헤더 배경 흰색
 
 
 //fav 버튼 클릭 시 채워지기 & 비워지기
@@ -24,16 +22,31 @@ favBtn.forEach(function(obj,ind,arr){
 })
 
 
-//햄버거 메뉴 클릭 시 nav page 뜨기
+//햄버거 메뉴 클릭 시 nav page 뜨기 & 슬라이드 효과
 const menuAll = document.querySelector('a.menu');
 const navPage = document.querySelector('#nav_page')
-console.log(menuAll)
+console.log(menuAll, navPage)
 let isMenu = false;
 
 menuAll.addEventListener('click',()=>{
     isMenu = !isMenu;
-    if(isMenu){
-        navPage.style = "display:flex"
-    }else {navPage.style = 'display:none'}
-    
+    navPage.style.transform = isMenu ? "translateY(0)" : "translateY(-440px)";
 })
+
+
+//scrollTrigger -> 헤더 배경 흰색
+const header = document.querySelector('#header')
+const bnrSection = document.querySelector('#bnr')
+console.log(header, bnrSection)
+
+window.addEventListener('scroll',()=>{
+    const scrollY = window.scrollY;
+    const bnrSectionEnd = bnrSection.offsetHeight;
+    if(scrollY>bnrSectionEnd){
+        header.style.backgroundColor = "#fff"
+    }else{header.style.backgroundColor = "transparent"}
+})
+
+//swiper pagination, btn
+
+
