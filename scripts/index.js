3fb91983e1dbfd4c6665ad015a1dc7485d82ac55
jjuglish <jjuglish@gmail.com>
2025-11-12 10:27:33 +0900

    에스더몰 메인페이지 css 완성

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+//햄버거 메뉴 클릭 시 nav page 뜨기
+//fav 버튼 클릭 시 채워지기 & 비워지기
+//swiper pagination, btn
+//서브메뉴 클릭 시 swiper 자체 변화,,, 추가 swiper 제작,, 애니메이션
+
+const favBtn = document.querySelectorAll('.fav')
+const favBtnImg = document.querySelector('.fav img')
+console.log(favBtn, favBtnImg)
