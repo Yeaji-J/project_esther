10f76b245dc59039af870f4bc9a8a3087c3f4391
jjuglish <jjuglish@gmail.com>
2025-11-27 15:25:05 +0900

    js swiper navigation 수정중

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -21,6 +21,7 @@ bnrSwiper.controller.control = bnrSwiper2;
 /* row2 best */
 const bestSwiper = new Swiper ("#best_prod_wrap",{
     slidesPerView:2,
+    loop:true,
     breakpoints : {
         740 : {
             slidesPerView:4,
@@ -31,7 +32,11 @@ const bestSwiper = new Swiper ("#best_prod_wrap",{
         1640 : {
             slidesPerView:4,
         }
-    }
+    },
+    navigation:{
+        prevEl:'#best_prod_wrap .swiper-button-prev',
+        nextEl:'#best_prod_wrap .swiper-button-next',
+    },
 });
 /* row4 featured */
 const featuredSwiper = new Swiper ("#featured_prod_wrap",{
@@ -43,14 +48,23 @@ const featuredSwiper = new Swiper ("#featured_prod_wrap",{
         850 : {
             slidesPerView:4,
         },
+    },
+    navigation:{
+        prevEl:'#featured_prod_wrap .swiper-button-prev',
+        nextEl:'#featured_prod_wrap .swiper-button-next',
     }
 })
 const projectSwiper = new Swiper ("#project_prod_wrap",{
     slidesPerView:2,
+    loop:true,
     breakpoints : {
         740 : {
             slidesPerView:2,
         },
         1024 : {slidesPerView:3},
+    },
+    navigation:{
+        prevEl:'#project_prod_wrap .swiper-button-prev',
+        nextEl:'#project_prod_wrap .swiper-button-next',
     }
 })
